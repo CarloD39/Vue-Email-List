d9befe4a99abe0f851a2a431cb87d9e14f2a7e50
CarloD39 <carlodurand@hotmail.it>
2021-07-08 19:51:27 +0200

    v-if

--- js/script.js
+++ js/script.js
@@ -6,20 +6,20 @@ new Vue (
 
         data:{
 
-            emails: []
+            emails: [],
         },
 
-        created () {
+        /*created () {
 
             this.getsEmails ()
 
-        },
+        },*/
 
         methods: {
 
             getsEmails: function() {
 
-                for (let i = 0; i < 5; i++ ) {
+                for (let i = 0; i < 10; i++ ) {
 
                     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                         .then((response) => {
